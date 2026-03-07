055faaa691632ed577c36d9e8cbdb5aa2f17ecd9
Taminul islam <taminulislam27@gmail.com>
2026-03-08 02:55:11 +0600

    Added Educational CF Round 187

---
+++ Educational Codeforces Round 187/A_Towers_of_Boxes.cpp
@@ -0,0 +1,50 @@
+/*
+        /\_/\.
+       (= ._.)
+       / >  \>
+._____________________..
+|                      |
+|   Md_Taminul_Islam   |
+|   CSE @ BU           |
+|______________________|
+*/#include<bits/stdc++.h>
+using namespace std;
+#define faster()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define for_i(a, b) for(int i = a; i < b; i++)
+#define for_n(a, b) for(int i = a; i >= b; i--)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << "\n"
+#define ll long long
+void tamin()
+{
+    int n, m, d; 
+    cin>>n>>m>>d; 
+    int div = d/m; 
+    if(div > n) 
+    {
+        cout<<1<<endl; 
+        return; 
+    }
+    else if(div == 0)
+    {
+        cout<<n<<endl; 
+        return; 
+    }
+    div++; 
+    cout<<n/div + (n%div == 0? 0: 1)<<endl; 
+}
+int main()
+{
+    faster();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        tamin();
+
+    return 0;
+}
