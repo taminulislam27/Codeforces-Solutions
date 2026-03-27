8f64fc0da4fedb364f92ecf7b5c9b6354a34a8bd
Taminul islam <taminulislam27@gmail.com>
2026-03-27 11:25:44 +0600

    #11

---
+++ CP-31 Sheet by TLE Eliminators/900/B_Not_Dividing.cpp
@@ -0,0 +1,51 @@
+/*
+        /\_/\.
+       (= ._.)
+       / >  \>
+._____________________..
+|                      |
+|   Md_Taminul_Islam   |
+|   CSE @ BU           |
+|______________________|
+*/#include<bits/stdc++.h>
+using namespace std;
+#define faster()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define for_i(a, b) for(int i = a; i < b; i++)
+#define for_n(a, b) for(int i = a; i >= b; i--)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << "\n"
+#define ll long long
+void tamin()
+{
+    ll n; cin>>n; 
+    vector<ll>a(n); 
+    for_i(0, n) cin>>a[i]; 
+    for_i(0, n-1){
+        if(i!=0 && a[i-1] == 2 && a[i] == 1){
+            a[i] += 2; 
+        } 
+        if(a[i] == 1) a[i]++; 
+        if(a[i+1]%a[i] == 0){
+            while (a[i+1]%a[i] == 0)
+            {
+                a[i+1]++; 
+            }
+        }
+    }
+    for_i(0, n) cout<<a[i]<<" "; nl; 
+}
+int main()
+{
+    faster();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        tamin();
+
+    return 0;
+}
