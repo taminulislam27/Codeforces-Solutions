2712e37feb6b6e59fdf78506f6c9e4f63eb0f967
Taminul islam <taminulislam27@gmail.com>
2026-03-27 11:27:04 +0600

    #9

---
+++ CP-31 Sheet by TLE Eliminators/900/B_Permutation_Swap.cpp
@@ -0,0 +1,44 @@
+/*
+        /\_/\.
+       (= ._.)
+       / >  \>
+._____________________..
+|                      |
+|   Md_Taminul_Islam   |
+|   CSE @ BU           |
+|______________________|
+*/#include<bits/stdc++.h>
+using namespace std;
+#define faster()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define for_i(a, b) for(int i = a; i < b; i++)
+#define for_n(a, b) for(int i = a; i >= b; i--)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << "\n"
+#define ll long long
+void tamin()
+{
+    int n; cin>>n; 
+    vector<int>a(n); 
+    for_i(0, n) cin>>a[i]; 
+    // int mx = abs(a[0]-1); 
+    int mx = 0; 
+    for_i(0, n){
+        mx = __gcd(mx, abs(a[i]-i-1)); 
+    }
+    cout<<mx<<endl; 
+}
+int main()
+{
+    faster();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        tamin();
+
+    return 0;
+}
