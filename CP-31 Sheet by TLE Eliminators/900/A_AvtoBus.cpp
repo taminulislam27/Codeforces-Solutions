19071377d2a4563db41a6ae985f458292253655d
Taminul islam <taminulislam27@gmail.com>
2026-04-01 12:57:10 +0600

    #14

---
+++ CP-31 Sheet by TLE Eliminators/900/A_AvtoBus.cpp
@@ -0,0 +1,43 @@
+/*
+        /\_/\.
+       (= ._.)
+       / >  \>
+._____________________..
+|                      |
+|   Md_Taminul_Islam   |
+|   CSE @ BU           |
+|______________________|
+*/#include<bits/stdc++.h>
+using namespace std;
+#define faster()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define for_i(a, b) for(int i = a; i < b; i++)
+#define for_n(a, b) for(int i = a; i >= b; i--)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << "\n"
+#define ll long long
+void tamin()
+{
+    ll n; cin>>n; 
+    double mn = 0, mx = 0; 
+    mn = ceil((double)n/6.0);
+    mx = n/4;  
+    if(n<4 || n%2 != 0)
+        cout<<-1<<endl; 
+    else   
+        cout<<(ll)mn<<" "<<(ll)mx<<endl; 
+}
+int main()
+{
+    faster();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        tamin();
+
+    return 0;
+}
